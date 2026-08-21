2d51231430a547248796ac3a9614aa14a661d89c
UTTAM CHAUDHARY <145982792+Chaudharyuttam@users.noreply.github.com>
2026-08-21 13:09:49 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isAlphanumeric(char ch){
+        if((ch >= '0' && ch <= '9')|| tolower(ch) >= 'a' && tolower(ch) <= 'z'){
+            return true;
+        }
+        return false;
+    }
+    bool isPalindrome(string s) {
+        int n = s.length();
+        int start = 0, end = n - 1;
+
+        while(start < end){
+            if(!isAlphanumeric(s[start])){
+                start++;
+                continue;
+            }
+            if(!isAlphanumeric(s[end])){
+                end--;
+                continue;
+            }
+            if(tolower(s[start]) != tolower(s[end])){
+               return false;
+            }
+            else{
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+};
