679a5210145785fd30430e00267e733a4f3bd8c5
UTTAM CHAUDHARY <145982792+Chaudharyuttam@users.noreply.github.com>
2026-09-08 11:54:28 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        
+        int n = position.size();
+
+        vector<pair<int, double>> road;
+
+        for(int i = 0; i < n; i++) {
+            double time = (double)(target - position[i]) / speed[i];
+            road.push_back({position[i], time});
+        }
+
+        // Sort cars from closest to target to farthest
+        sort(road.begin(), road.end(),
+             [](const pair<int, double>& a,
+                const pair<int, double>& b) {
+                    return a.first > b.first;
+             });
+
+        int fleets = 0;
+        double lastTime = 0;
+
+        // Check cars from front to back
+        for(int i = 0; i < n; i++) {
+
+            double currTime = road[i].second;
+
+            // This car cannot catch the fleet in front
+            if(currTime > lastTime) {
+                fleets++;
+                lastTime = currTime;
+            }
+        }
+
+        return fleets;
+    }
+};
